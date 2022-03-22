a922d9ce46fa8cf1f99609f52ef3172096062ff0
Keenah <kvancamp@mail.com>
2022-03-21 21:24:29 -0500

    started building migration

--- app-goodbook/db/migrate/20220322014644_create_good_books.rb
+++ app-goodbook/db/migrate/20220322014644_create_good_books.rb
@@ -1,7 +1,13 @@
 class CreateGoodBooks < ActiveRecord::Migration
   def change
-    create_table :good_books do |t|
-
+    create_table :books do |t|
+      t.string :title
+      t.string :author 
+      t.string :photo_address
+      t.text :description
+      t.string :category
+      t.integer :rating  
+      
       t.timestamps null: false
     end
   end
